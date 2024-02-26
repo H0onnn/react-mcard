79c8e488b4b79cafaa9ba8b8cfe041d6619b87db
H0onnn <dukei2017@naver.com>
2024-02-26 17:57:00 +0900

    Feat: common components

---
+++ src/contexts/AlertContext/index.tsx
@@ -0,0 +1,76 @@
+import {
+  ComponentProps,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from 'react'
+
+import { createPortal } from 'react-dom'
+
+import Alert from '@shared/Alert'
+
+type AlertProps = ComponentProps<typeof Alert>
+type AlertOptions = Omit<AlertProps, 'open'>
+
+interface AlertContextValue {
+  open: (options: AlertOptions) => void
+}
+
+const Context = createContext<AlertContextValue | undefined>(undefined)
+
+const defaultValues: AlertProps = {
+  open: false,
+  title: null,
+  description: null,
+  onButtonClick: () => {},
+}
+
+export const AlertContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  const [alertState, setAlertState] = useState(defaultValues)
+
+  const $portal = document.getElementById('root-portal')
+
+  const close = useCallback(() => {
+    setAlertState(defaultValues)
+  }, [])
+
+  const open = useCallback(
+    ({ onButtonClick, ...options }: AlertOptions) => {
+      setAlertState({
+        ...options,
+        onButtonClick: () => {
+          close()
+          onButtonClick()
+        },
+        open: true,
+      })
+    },
+    [close],
+  )
+
+  const values = useMemo(() => ({ open }), [open])
+
+  return (
+    <Context.Provider value={values}>
+      {children}
+      {$portal && createPortal(<Alert {...alertState} />, $portal)}
+      <Alert {...alertState} />
+    </Context.Provider>
+  )
+}
+
+export const useAlertContext = () => {
+  const values = useContext(Context)
+
+  if (!values) {
+    throw new Error('AlertContext 내부에서 사용해주세요.')
+  }
+
+  return values
+}

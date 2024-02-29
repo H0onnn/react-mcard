a00c9ed3f0882a6418212feb1ab960a2d32b97b4
H0onnn <dukei2017@naver.com>
2024-02-29 17:49:36 +0900

    Feat: Navbar 분기처리

--- src/components/shared/NavBar/index.tsx
+++ src/components/shared/NavBar/index.tsx
@@ -1,17 +1,26 @@
 /** @jsxImportSource @emotion/react */
 
+import { useLocation } from 'react-router-dom'
 import { css } from '@emotion/react'
 import { Link } from 'react-router-dom'
 import Flex from '@shared/Flex'
 import Button from '@shared/Button'
+import { colors } from '@/styles/colorPalette'
 
 const NavBar = () => {
+  const location = useLocation()
+
+  const showSignButton =
+    ['/signup', '/signin'].includes(location.pathname) === false
+
   return (
     <Flex justify="space-between" align="center" css={navbarContainerStyles}>
       <Link to="/">홈</Link>
-      <Link to="/signup">
-        <Button>로그인/회원가입</Button>
-      </Link>
+      {showSignButton && (
+        <Link to="/signup">
+          <Button>로그인/회원가입</Button>
+        </Link>
+      )}
     </Flex>
   )
 }
@@ -20,4 +29,10 @@ export default NavBar
 
 const navbarContainerStyles = css`
   padding: 10px 24px;
+
+  position: sticky;
+  top: 0;
+  background-color: ${colors.white};
+  z-index: 10;
+  border-bottom: 1px solid ${colors.grey};
 `
